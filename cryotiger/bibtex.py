b0b72f070b353e3ad32a54abc89b719c795033bb
Ratolon <miceta@cnb.csic.es>
2025-09-07 14:46:24 +0000

    Bibliography add

---
+++ cryotiger/bibtex.py
@@ -0,0 +1,36 @@
+# **************************************************************************
+# *
+# * Author:    EP Murillo (ep.murillo@usp.ceu.es)
+# * Author:    Mikel iceta (miceta@cnb.csic.es)
+# *
+# * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 3 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+"""
+@article{Turonova2024,
+  title = "cryoTIGER: Deep-Learning Based Tilt Interpolation Generator for Enhanced Reconstruction in Cryo Electron Tomography",
+  author = {Tomas Majtner, Jan Philipp Kreysing, Maarten W.Tuijtel, Sergio Cruz-Leon, Jiasui Liu, Gerhard Hummer, Martin Beck, Beata Turonova},
+  journal = "BioRxiv",
+  year = "2024",
+  doi = "https://doi.org/10.1101/2024.12.17.628939",
+  url = "https://www.biorxiv.org/content/10.1101/2024.12.17.628939v1",
+}
+"""
